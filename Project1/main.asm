35b1aa7e3da07ca7e2c447be56718ea9e5ef80a7
Roye <royeabelman@gmail.com>
2018-06-10 18:48:26 +0300

    ba

--- Project1/main.asm
+++ Project1/main.asm
@@ -1,6 +1,6 @@
 include \masm32\include\masm32rt.inc
 include mainfuncs.inc
-
+;This is the main file. This is where masm will start reading the code. The code invokes the init function and then loops between update and draw
 .CODE
 	main PROC
 		INVOKE init
